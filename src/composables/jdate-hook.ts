5690c749b5d3857802e429562b15c177944e92ef
Amir Ali Hemmati <amir.negative21@gmail.com>
2024-03-17 20:38:02 +0330

    feat(hook): initial useJalaliMask hook

--- src/composables/jdate-hook.ts
+++ src/composables/jdate-hook.ts
@@ -1,36 +1,24 @@
-import { computed, ref } from "vue";
 import moment from "moment-jalaali";
-import IMask from 'imask';
-
+import { IMask, useIMask } from 'vue-imask'
 const useJalaliInputMask = () => {
-  // const {el, masked, unmasked} = useIMask({
-  //   mask: ,
-  //   pattern: 'jYYYY/jMM/jDD',
-  //   expose: true,
-  //   lazy: false,
-  //   format: date => moment(date).format('jYYYY/jMM/jDD'),
-  //   parse: str => moment(str, 'jYYYY/jMM/jDD').toDate(),
-  //   blocks: {
-  //     jYYYY: {
-  //       mask: IMask.MaskedRange,
-  //       from: 1,
-  //       to: props.lockFuture ? Number(moment().format('jYYYY')) : 9999
-  //     },
-  //     jMM: {
-  //       mask: IMask.MaskedRange,
-  //       from: 1,
-  //       to: props.lockFuture ? Number(moment().format('jMM')) : 12
-  //     },
-  //     jDD: {
-  //       mask: IMask.MaskedRange,
-  //       from: 1,
-  //       to: props.lockFuture ? Number(moment().format('jDD')) : 31
-  //     },
-  //   }
-  // }, {
-  //   onComplete() {
-  //   }
-  // });
+  const {el, masked, unmasked} = useIMask({
+    mask: IMask.MaskedDate,
+    pattern: 'jYYYY/jMM/jDD',
+    expose: true,
+    lazy: false,
+    parse: str => moment(str, 'jYYYY/jMM/jDD').toDate(),
+    blocks: {
+      jYYYY: {
+        mask: IMask.MaskedNumber,
+      },
+      jMM: {
+        mask: IMask.MaskedNumber,
+      },
+      jDD: {
+        mask: IMask.MaskedNumber,
+      },
+    }
+  });
 
   return {
     el,
